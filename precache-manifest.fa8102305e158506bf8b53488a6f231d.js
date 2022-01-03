be7c560f4468d2015e826f082e6248e7f3d3b788
David CM <dhf360@gmail.com>
2022-01-03 15:27:41 -0600

    Update

---
+++ precache-manifest.fa8102305e158506bf8b53488a6f231d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425547993bb84d707c0b",
+    "url": "/whatLink/css/app.b702c677.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/whatLink/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "741cf587c7222c66f03e6e1dc5fb6745",
+    "url": "/whatLink/index.html"
+  },
+  {
+    "revision": "425547993bb84d707c0b",
+    "url": "/whatLink/js/app.347e020b.js"
+  },
+  {
+    "revision": "940e15935970b69b0fde",
+    "url": "/whatLink/js/chunk-vendors.b1d76bae.js"
+  },
+  {
+    "revision": "ac402379994811d7979edf15a58d22cb",
+    "url": "/whatLink/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/whatLink/robots.txt"
+  }
+]);
